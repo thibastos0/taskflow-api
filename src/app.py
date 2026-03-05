adc4791ada8dae7b2856fac9a0a5e396a6cadd83
thibastos0 <thibastos@live.com>
2026-03-04 21:43:53 -0300

    feat: adiciona rota /usuarios

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
 
@@ -9,6 +10,10 @@ def home():
 @app.route("/status")
 def status():
     return {"status": "API online"}
+
+@app.route("/usuarios")
+def usuarios():
+    return jsonify(["Ana", "Carlos", "Maria"])
 
 if __name__ == '__main__':
     app.run(debug=True)
