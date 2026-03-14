d23409d257053dea2a565fac72baa70dd4ee2204
thibastos0 <thibastos@live.com>
2026-03-14 19:42:42 -0300

    Atualiza título da página inicial

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Sistema de Gerenciamento de Biblioteca"
+    return "Sistema de Gestão de Biblioteca"
 
 @app.route("/status")
 def status():
